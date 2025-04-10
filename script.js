67ef56293812c4f46e299181310d1d76f2157f4d
abhishek-web-design <abhishekvadnala10@gmail.com>
2025-04-10 18:57:09 +0530

    Initial commit - Reverse-A-String

---
+++ script.js
@@ -0,0 +1,9 @@
+let str="JavaScript";
+let letters=str.split("");
+console.log(letters);
+
+let reversedArray=letters.reverse();
+console.log(reversedArray);
+
+let joinArray=reversedArray.join("");
+console.log(joinArray);
